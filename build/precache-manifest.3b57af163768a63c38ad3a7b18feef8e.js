4ae06c9924b42d5762873f14d1d8af36f22bf519
Mo2024 <mohdosama2025@gmail.com>
2022-02-22 07:21:14 +0300

    Website deployed

---
+++ build/precache-manifest.3b57af163768a63c38ad3a7b18feef8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb6d48d5c933c543293d",
+    "url": "/react-lights-out/static/css/main.01c80463.chunk.css"
+  },
+  {
+    "revision": "bb6d48d5c933c543293d",
+    "url": "/react-lights-out/static/js/main.bb6d48d5.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/react-lights-out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "2e2091384c069a0f9f245ada09ab629b",
+    "url": "/react-lights-out/index.html"
+  }
+];
